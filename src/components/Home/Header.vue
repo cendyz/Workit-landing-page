0cbe069b260ca8a0b63f3155d3ebac335c86a389
cendyz <arek57823@gmail.com>
2025-01-30 18:59:41 +0100

    header

--- src/components/Home/Header.vue
+++ src/components/Home/Header.vue
@@ -1,6 +1,6 @@
 <template>
 	<header
-		class="bg-purple-900 text-center px-[2rem] pt-[5.6rem] pb-[14.4rem]">
+		class="bg-purple-900 text-center px-[2rem] pt-[5.6rem] pb-[16.8rem] relative before:absolute before:bottom-[-3rem] before:left-1/2 before:w-[130%] before:translate-x-[-50%] before:h-[20rem] before:bg-purple-900">
 		<h1
 			class="text-[4.8rem] text-white font-fraunces font-thin leading-[1.1] mb-[4rem]">
 			Data
@@ -15,5 +15,21 @@
 			class="px-[3.2rem] py-[1.2rem] bg-green text-[1.6rem] font-bold">
 			Learn more
 		</button>
+		<img
+			:src="phone"
+			alt="phone"
+			class="w-[33rem] absolute bottom-[-10rem] left-1/2 translate-x-[-50%]" />
 	</header>
 </template>
+
+<script setup lang="ts">
+import phone from '@/images/illustration.png'
+</script>
+
+<style lang="scss">
+header {
+	&::before {
+		clip-path: ellipse(50% 36% at 50% 63%);
+	}
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,10 @@
 <template>
-	<nav class="px-[1.25em] pt-[2em] flex justify-between items-center bg-purple-900">
-		<img :src="logo" alt="workit" class="w-[5.985em]" />
+	<nav
+		class="px-[2rem] pt-[3.2rem] flex justify-between items-center bg-purple-900 relative z-10">
+		<img :src="logo" alt="workit" class="w-[9.6rem]" />
 		<a
 			href="#"
-			class="text-[1.8rem] underline decoration-[3px] underline-offset-8 decoration-green text-white font-bold"
+			class="text-[1.8rem] underline decoration-[3px] underline-offset-8 decoration-green text-white font-bold z-100"
 			>Apply for access</a
 		>
 	</nav>
